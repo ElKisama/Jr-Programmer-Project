d1199bc64c6f70043723cf2597ad10224a0fb201
Marcelo Gangas <54752452+ElKisama@users.noreply.github.com>
2022-09-25 22:39:58 -0300

    Implement Data Persistence between sessions

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class MainManager : MonoBehaviour
@@ -9,7 +10,35 @@ public class MainManager : MonoBehaviour
     // Every MainManager instance on the scene will share the same Instance variable.
     public static MainManager Instance;
     public Color TeamColor;
+    
+    [System.Serializable]
+    class SaveData
+    {
+        public Color TeamColor;
+    }
 
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.TeamColor = TeamColor;
+
+        string json = JsonUtility.ToJson(data);
+        
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+    
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            TeamColor = data.TeamColor;
+        }
+    }
+    
     private void Awake()
     {
         if (Instance != null)
@@ -20,5 +49,6 @@ public class MainManager : MonoBehaviour
         
         Instance = this;
         DontDestroyOnLoad(gameObject);
+        LoadColor();
     }
 }
